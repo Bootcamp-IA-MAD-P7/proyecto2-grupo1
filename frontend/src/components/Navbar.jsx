de2002abcc54af07259473e2cff5e7672aa097a9
Arinbasco <abasconsuelo@gmail.com>
2026-05-28 15:20:02 +0200

    Feat: react dependencies and frontend  files structure

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar() {
+    return (
+        <nav>
+        <Link to="/">Gmail</Link>
+        <Link to="/imagenes">Imágenes</Link>
+        <img src="avatar.jpg" />
+        <button>Sign in</button>
+    </nav>
+    )
+}
