37398de40fec9b69619e2bf491d7de1aef984196
Arinbasco <abasconsuelo@gmail.com>
2026-06-03 09:23:11 +0200

    feat: AuthContext con login, logout y rol en Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,24 @@
 import { Link } from 'react-router-dom'
 import logoImg from '../assets/logo_musintage.png' // <-- Cambiamos .jpg por .png
 import { useSearch } from '../context/SearchContext'
+import { useAuth } from '../context/AuthContext'
+import { useNavigate } from 'react-router-dom'
 
 function Navbar() {
   const { search, setSearch } = useSearch()
+  const { token, rol, logout } = useAuth()
+  const navigate = useNavigate()
+
+  function handleLogout() {
+    logout()
+    navigate('/')
+  }
+
   return (
     <nav>
       <Link to="/">
         <img src={logoImg} alt="Musvintage" />
       </Link>
-      <Link to="/catalogo">Catálogo</Link>
       <Link to="/login">IniciarSecion</Link>
       <Link to="/register">Registro</Link>
       <input
